19e1ecd9b2f8f153c735c0fd2c247489a22c4c8c
devopsnarsh87 <devopsnaresh87@gmail.com>
2024-06-27 17:41:57 +0530

    added 2nd natgw

--- dev/nat.tf
+++ dev/nat.tf
@@ -3,3 +3,9 @@ module "dev_natgw_1" {
   public_subnet_id = module.dev_vpc_1.public_subnets_id_1
   vpc_name         = module.dev_vpc_1.vpc_name
 }
+
+module "dev_natgw_2" {
+  source = "../modules/nat"
+  public_subnet_id = module.dev_vpc_2.public_subnet_id_2
+  vpc_name = module.dev_vpc_2.vpc_name
+}
